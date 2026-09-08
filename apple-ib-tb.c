aea48fa45aed51548a0a8122ddbf4a2a5e1482cd
Phennova <sorenaclinton@gmail.com>
2026-09-08 10:21:23 +0530

    Copyright Edit
    Added compatibility fixes for kernel 6.11+ and handled hid-sensor-hub bind conflicts.

--- apple-ib-tb.c
+++ apple-ib-tb.c
@@ -3,6 +3,9 @@
  * Apple Touch Bar Driver
  *
  * Copyright (c) 2017-2018 Ronald Tschalär
+ *
+ * Modified 2026 by Phennova: kernel 6.11+ API compatibility fixes,
+ * hid-sensor-hub bind-conflict handling, mac_mode config-2 switch.
  */
 
 /*
